52689aa9cb2e30c51c681d779ded8f2a4f7d3b2e
NintendoShazam <cferguson575@yahoo.com>
2019-11-12 16:50:33 -0500

    Edited the works cited file

--- src/WorksCited.js
+++ src/WorksCited.js
@@ -59,31 +59,31 @@ export class WorksCited extends React.Component{
                     <Card.Title id="works-cited-title">Works Cited</Card.Title>
                     <Card.Text>
                       <div className="works-cited">
-                        <h4 className="cited-source">AFGuidesHD. “Spider-Man PS4 - Pizza Time.” YouTube, YouTube, 22 Oct. 2018, https://www.youtube.com/watch?v=qh9yn9vGNzg.
-                        Accessed 5 November 2019</h4>
-                        <h4 className="cited-source">Ayres, Brice. “Build a Website with React, React-Bootstrap, React-Router and Styled-Components.”
-                         YouTube, YouTube, https://www.youtube.com/watch?v=tOK9l5uP06U&t=71s. </h4>
-                        <h4 className="cited-source">"Can't Resolve react-router-dom #231" GitHub,
-                          https://github.com/react-bootstrap/react-router-bootstrap/issues/231 </h4>
-                        <h4 className="cited-source">Luce, Ivan De. “Pizza Hut Is Rebooting Its Iconic 'Red Roof' Logo with a Retro Design.” Business Insider,
-                           Business Insider, 27 June 2019, https://www.businessinsider.com/pizza-hut-brings-back-old-logo-2019-6. Accessed 6 November 2019.</h4>
-                        <h4 className="cited-source">Lutz, Ashley. "Here's A Pie Chart Of The Most Popular Pizza Toppings" Business Insider, Business Insider,
+                        <p className="cited-source">AFGuidesHD. “Spider-Man PS4 - Pizza Time.” YouTube, YouTube, 22 Oct. 2018, https://www.youtube.com/watch?v=qh9yn9vGNzg.
+                        Accessed 5 November 2019</p>
+                        <p className="cited-source">Ayres, Brice. “Build a Website with React, React-Bootstrap, React-Router and Styled-Components.”
+                         YouTube, YouTube, https://www.youtube.com/watch?v=tOK9l5uP06U&t=71s. </p>
+                        <p className="cited-source">"Can't Resolve react-router-dom #231" GitHub,
+                          https://github.com/react-bootstrap/react-router-bootstrap/issues/231 </p>
+                        <p className="cited-source">Luce, Ivan De. “Pizza Hut Is Rebooting Its Iconic 'Red Roof' Logo with a Retro Design.” Business Insider,
+                           Business Insider, 27 June 2019, https://www.businessinsider.com/pizza-hut-brings-back-old-logo-2019-6. Accessed 6 November 2019.</p>
+                        <p className="cited-source">Lutz, Ashley. "Here's A Pie Chart Of The Most Popular Pizza Toppings" Business Insider, Business Insider,
                                                       10 October 2014, https://www.businessinsider.com/the-most-popular-pizza-toppings-chart-2013-10. Accessed
-                                                      7 November 2019.</h4>
-                        <h4 className="cited-source">Muffin, “Baked Pizza” Pexels, https://www.pexels.com/photo/baked-pizza-1653772/.</h4>
-                        <h4 className="cited-source">“Picks for $6 - Mix & Match Pizza and Sides.” Papa John's, https://www.papajohns.com/order/menu/pizza/picks-for-6.
-                         Accessed 1 November 2019.</h4>
-                        <h4 className="cited-source">(Note: I used 3 pictures) Pixabay, “Cheese Close Up Food. Vegetables Italian Pizza. Pepperoni Pizza” Pexels,
-                         https://www.pexels.com/@pixabay. 30 October 2019.</h4>
-                        <h4 className="cited-source">"Pizza Hut - We're Ready for the NFL Draft, Are You?" Pizza Hut
-                        , https://www.facebook.com/pizzahutus/posts/were-ready-for-the-nfl-draft-are-you-/10155822457947415/. Accessed 26 October 2019</h4>
-                        <h4 className="cited-source">SmileMakers, “Teenage Mutant Ninja Turtles Pin the Pizza Party Game - Birthday and Theme Party Supplies By SmileMakers.”
+                                                      7 November 2019.</p>
+                        <p className="cited-source">Muffin, “Baked Pizza” Pexels, https://www.pexels.com/photo/baked-pizza-1653772/.</p>
+                        <p className="cited-source">“Picks for $6 - Mix & Match Pizza and Sides.” Papa John's, https://www.papajohns.com/order/menu/pizza/picks-for-6.
+                         Accessed 1 November 2019.</p>
+                        <p className="cited-source">(Note: I used 3 pictures) Pixabay, “Cheese Close Up Food. Vegetables Italian Pizza. Pepperoni Pizza” Pexels,
+                         https://www.pexels.com/@pixabay. 30 October 2019.</p>
+                        <p className="cited-source">"Pizza Hut - We're Ready for the NFL Draft, Are You?" Pizza Hut
+                        , https://www.facebook.com/pizzahutus/posts/were-ready-for-the-nfl-draft-are-you-/10155822457947415/. Accessed 26 October 2019</p>
+                        <p className="cited-source">SmileMakers, “Teenage Mutant Ninja Turtles Pin the Pizza Party Game - Birthday and Theme Party Supplies By SmileMakers.”
                          Walmart.com, https://www.walmart.com/ip/Teenage-Mutant-Ninja-Turtles-Pin
-                         -the-Pizza-Party-Game-Birthday-and-Theme-Party-Supplies-By-SmileMakers/252694845. Accessed 1 November 2019.</h4>
-                         <h4 className="cited-source">Styled Components. “Styled-Components.” Styled Components, https://www.styled-components.com/.</h4>
-                         <h4 className="cited-source"> Little Caesars, "Classic Pizzas." https://littlecaesars.com/en-us/. Accessed 8 November 2019.</h4>
-                         <h4 className="cited-source"> Papa John's, "Sausage Pizza." https://www.papajohns.com/order/menu/pizza/sausage-pizza.
-                          Accessed 7 November 2019.</h4>
+                         -the-Pizza-Party-Game-Birthday-and-Theme-Party-Supplies-By-SmileMakers/252694845. Accessed 1 November 2019.</p>
+                         <p className="cited-source">Styled Components. “Styled-Components.” Styled Components, https://www.styled-components.com/.</p>
+                         <p className="cited-source"> Little Caesars, "Classic Pizzas." https://littlecaesars.com/en-us/. Accessed 8 November 2019.</p>
+                         <p className="cited-source"> Papa John's, "Sausage Pizza." https://www.papajohns.com/order/menu/pizza/sausage-pizza.
+                          Accessed 7 November 2019.</p>
                       </div>
                     </Card.Text>
                   </Card.Body>
